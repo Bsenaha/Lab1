f6361018e48414333f0527bd1e3b9ebff9e6cb62
Brandon Senaha <145088993+Bsenaha@users.noreply.github.com>
2024-07-18 18:33:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Brandon Senaha
+Lab 1
+7/1/2024 
+'''
+# print statements
+print("Full Name: Brandon Senaha")
+print("Student ID: 20510375")
+print("Email: brandonsenaha@gmail.com")
+print('\n',
+      "Three Course Policies:", '\n'
+      "1. The subject line of an email to the instructor MUST include course",'\n'
+      "   number and student name. This facilitates a timely response and avoids",'\n'
+      "   student emails from being filtered out.",'\n'
+      "2. Students who feel their work has been improperly graded may request",'\n'
+      "   a regrade. This request must be made within a week of said grade.",'\n'
+      "3. It is a student's responsibility to drop himself/herself from the class.",'\n'
+      "    Simply being absent from all the class activities will result in an F.")
